2f5a936ce35313571c2d503fff778de81bd4a820
Mario Dambros Filho <dambros@gmail.com>
2015-10-21 04:00:11 -0200

    progressao

--- src/main/java/br/com/loducca/clusterer/utils/BoundingBoxUtils.java
+++ src/main/java/br/com/loducca/clusterer/utils/BoundingBoxUtils.java
@@ -7,6 +7,7 @@ import static java.lang.Math.asin;
 import static java.lang.Math.atan2;
 import static java.lang.Math.cos;
 import static java.lang.Math.sin;
+import static java.lang.Math.sqrt;
 import static java.lang.Math.toDegrees;
 import static java.lang.Math.toRadians;
 
@@ -20,7 +21,7 @@ public class BoundingBoxUtils {
 	private static final double BOTTOM_RIGHT_ANGLE = 135;
 	private static final double BOTTOM_LEFT_ANGLE = 225;
 	private static final double TOP_LEFT_ANGLE = 315;
-	private static final double RADIUS = 6378.1; // KMs
+	private static final double RADIUS = 6371; // KMs
 
 	//calculate a square boundingbox
 	public static BoundingBox calculate(Marker centerMarker, double distance) {
@@ -28,7 +29,7 @@ public class BoundingBoxUtils {
 		Marker tempBottomRight = getBoundaryMarker(centerMarker, distance, BOTTOM_RIGHT_ANGLE);
 		Marker tempTopLeft = getBoundaryMarker(centerMarker, distance, TOP_LEFT_ANGLE);
 		Marker tempBottomLeft = getBoundaryMarker(centerMarker, distance, BOTTOM_LEFT_ANGLE);
-
+//
 		double minLat = tempTopLeft.getLat() < tempBottomLeft.getLat() ? tempTopLeft.getLat() : tempBottomLeft.getLat();
 		double maxLat = tempTopLeft.getLat() > tempBottomLeft.getLat() ? tempTopLeft.getLat() : tempBottomLeft.getLat();
 		double minLng = tempTopLeft.getLng() < tempTopRight.getLng() ? tempTopLeft.getLng() : tempTopRight.getLng();
@@ -39,6 +40,25 @@ public class BoundingBoxUtils {
 		Marker topLeft = new Marker(maxLat, minLng);
 		Marker bottomLeft = new Marker(minLat, minLng);
 
+
+//		double latDistance = distance / 111.2;
+//		double lngDistance = 111.320 * cos(latDistance);
+
+//		Marker bottomLeft = new Marker((centerMarker.getLat() - latDistance / 2), (centerMarker.getLng() - latDistance / 2));
+//		Marker bottomRight = new Marker((centerMarker.getLat() - latDistance / 2), (centerMarker.getLng() + latDistance / 2));
+//		Marker topRight = new Marker((centerMarker.getLat() + latDistance / 2), (centerMarker.getLng() + latDistance / 2));
+//		Marker topLeft = new Marker((centerMarker.getLat() + latDistance / 2), (centerMarker.getLng() - latDistance / 2));
+
+
+//		double latDistance = distance / 110.54;
+//		double lngDistance = 111.320 * cos(centerMarker.getLat());
+//
+//		Marker topRight = new Marker(centerMarker.getLat() + latDistance, centerMarker.getLng() + lngDistance);
+//		Marker topLeft = new Marker(centerMarker.getLat() + latDistance, centerMarker.getLng() - lngDistance);
+//		Marker bottomRight = new Marker(centerMarker.getLat() - latDistance, centerMarker.getLng() + lngDistance);
+//		Marker bottomLeft = new Marker(centerMarker.getLat() - latDistance, centerMarker.getLng() - lngDistance);
+
+//		return new BoundingBox(tempTopRight, tempBottomRight, tempTopLeft, tempBottomLeft);
 		return new BoundingBox(topRight, bottomRight, topLeft, bottomLeft);
 	}
 
@@ -48,12 +68,13 @@ public class BoundingBoxUtils {
 
 	private static double getLongitude(double distance, double lat, double lng, double angle) {
 		double newLat = getLatitude(distance, lat, angle);
-		return toDegrees(toRadians(lng) + atan2(sin(toRadians(angle)) * sin(distance / RADIUS) * cos(toRadians(lat)), cos(distance / RADIUS) - sin(toRadians(lat)) * sin(toRadians(lat))));
+		return toDegrees(toRadians(lng) + atan2(sin(toRadians(angle)) * sin(distance / RADIUS) * cos(toRadians(lat)), cos(distance / RADIUS) - sin(toRadians(lat)) * sin(toRadians(newLat))));
 	}
 
 	private static Marker getBoundaryMarker(Marker centerMarker, double distance, double angle) {
-		double lat = getLatitude(distance, centerMarker.getLat(), angle);
-		double lng = getLongitude(distance, centerMarker.getLat(), centerMarker.getLng(), angle);
+		double d = sqrt((distance * distance) + (distance * distance));
+		double lat = getLatitude(d, centerMarker.getLat(), angle);
+		double lng = getLongitude(d, centerMarker.getLat(), centerMarker.getLng(), angle);
 		return new Marker(lat, lng);
 	}
 }
